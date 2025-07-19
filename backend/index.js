14bcf86f31e916075517bd7cc83ff7d1a6b89ee3
janidu chamika <janchamika1@gmail.com>
2025-07-20 01:01:53 +0530

    handlin user login  via jwt
    access home only logined users

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 import { connectToDatabase } from './lib/db.js';
 const app = express();
 
@@ -23,7 +24,7 @@ app.post('/register', async (req, res) => {
             console.error(err);
             return res.status(500).json(err);
       }
-      console.log(username);
+
 });
 
 app.post('/login', async (req, res) => {
@@ -38,14 +39,46 @@ app.post('/login', async (req, res) => {
             if (!isMatch) {
                   return res.status(401).json({ message: "wrong-info" });
             }
-
-            return res.status(201).json({ message: "Login successfully" });
+            const token = jwt.sign({ id: rows[0].id }, process.env.JWT_KEY, { expiresIn: '3h' });
+            return res.status(201).json({ token: token });
       } catch (err) {
             console.error(err);
-            res.status(500).json(err);
+            return res.status(500).json(err);
       }
       console.log(username);
 });
+
+// verify is there token (check user is logined)
+const verifyToken = async (req, res, next) => {
+      try {
+            const token = req.headers['authorization'].split(' ')[1];
+            if (!token) {
+                  return res.status(403).json({ message: "access denide" });
+            }
+            const decoded = jwt.verify(token, process.env.JWT_KEY);
+            req.userId = decoded.id;
+            next();
+      } catch (error) {
+            // console.log(err);
+            return res.status(500).json({ message: "server error" });
+      }
+};
+
+app.get('/home', verifyToken, async (req, res) => {
+      try {
+            const db = await connectToDatabase();
+            const [rows] = await db.query('SELECT * FROM user WHERE id=?', [req.userId]);
+            if (rows.length === 0) {
+                  return res.status(404).json({ message: "User not exits" });
+            }
+
+            return res.status(201).json({ user: rows[0] });
+      } catch (err) {
+            // console.log(err);
+            return res.status(500).json(err);
+      }
+});
+
 app.listen(process.env.PORT, () => {
       console.log("Server is running..");
 })
